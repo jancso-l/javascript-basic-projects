3bbf8c7831e015e64ea8ace5d60b2df834014be7
jancso-l <61355854+jancso-l@users.noreply.github.com>
2025-05-03 22:53:42 +0200

    almost done

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -71,4 +71,63 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "stake dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
+
+const sectionCenter = document.querySelector(".section-center");
+
+const filterBtns = document.querySelectorAll(".filter-btn");
+
+window.addEventListener("DOMContentLoaded", ()=>{
+  displayMenuItems(menu);  
+  const categories = menu.reduce((values, item)=>{
+    if (!values.includes(item.category)) {
+      values.push(item.category);
+    }
+    return values;
+  },
+  ["all"]
+  ); 
+  });
+
+
+
+filterBtns.forEach((btn) =>{
+  btn.addEventListener("click", (e)=>{
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter((menuItem) =>{
+      if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+      if (category === "all") {
+      displayMenuItems(menu);
+      }else{
+        displayMenuItems(menuCategory);
+      }
+  });
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map((item) =>{
+    return `<article class="menu-item">
+          <img src="${item.img}" class="photo" alt="${item.title}">
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">${item.price}</h4>
+            </header>
+            <p class="item-text">${item.desc}</p>
+          </div>
+        </article>`   
+      });
+      displayMenu = displayMenu.join("");
+      sectionCenter.innerHTML = displayMenu;  
+};
